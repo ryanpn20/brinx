a4acef34620b4e44ece08d865793f33644d9f14e
Ryan Nelson <ryanpn20@gmail.com>
2017-10-14 19:38:39 -0400

    topic new

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 20171013192655) do
 
   create_table "comments", force: :cascade do |t|
+    t.integer "unit_id"
+    t.integer "user_id"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
