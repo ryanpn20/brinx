ab6d4ba3a06be1fa699958baf01e2b144a5b5da2
Angel Rodriguez <copcos@gmail.com>
2017-10-14 15:22:25 -0400

    cualquier cosa

--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,6 @@ ActiveRecord::Schema.define(version: 20171013192655) do
   create_table "instructors", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "phone", limit: 8
