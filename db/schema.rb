d8c3ca575b77d7995083b9a09deba6bd1caf05b2
Angel Rodriguez <copcos@gmail.com>
2017-10-14 19:39:43 -0400

    courses user

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 20171013192655) do
 
   create_table "comments", force: :cascade do |t|
+    t.integer "unit_id"
+    t.integer "user_id"
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,6 +41,10 @@ ActiveRecord::Schema.define(version: 20171013192655) do
   create_table "instructors", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "phone", limit: 8
@@ -74,7 +81,11 @@ ActiveRecord::Schema.define(version: 20171013192655) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "phone", limit: 8
