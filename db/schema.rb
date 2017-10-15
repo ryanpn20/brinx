ddeb7e504b8da917e546522d762a1a7b3b219626
mario diaz <mdiaz00147@gmail.com>
2017-10-15 12:53:59 -0400

    r

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013192655) do
+ActiveRecord::Schema.define(version: 20171015132550) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "unit_id"
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(version: 20171013192655) do
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "logo_file_name"
+    t.string "logo_content_type"
+    t.integer "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   create_table "units", force: :cascade do |t|
