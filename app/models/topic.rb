53dea3e7b5fa70eedf5999d165021736de06b66a
mario diaz <mdiaz00147@gmail.com>
2017-10-15 17:56:03 -0400

    topics almost done

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,17 @@
 class Topic < ApplicationRecord
+	before_save :set_status
+
 	has_many :courses
 	has_many :commitments
 
 	has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://www.1plusx.com/app/mu-plugins/all-in-one-seo-pack-pro/images/default-user-image.png"
 	validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
+	validates :name, presence: true
+	validates :cat, presence: true
+	validates :language, presence: true
+	validates :description, presence: true
+
+	def set_status
+		self.status = true
+	end
 end
