8bafa164bd9faa567b6460276f7669c388eb1a31
mario diaz <mdiaz00147@gmail.com>
2017-10-16 14:35:56 -0400

    r

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,7 @@
 module CoursesHelper
+	def get_total_time(course)
+		course = course.units.map { |x| x.duration }
+		course = course.reduce(0, :+)
+		Time.at(course).utc.strftime("%H:%M:%S")
+	end
 end
